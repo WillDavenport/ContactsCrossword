5066028c0a97765888ec09cb0109f8fb2cef4dd1
William Davenport <willdavenport2015@gmail.com>
2020-03-31 19:48:51 -0700

    second build

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 import { generateCrossword } from './services/generateCrossword';
 import CrosswordView from './components/crosswordView';
 import * as Contacts from 'expo-contacts';
 import { trimContactsData } from './services/trimContacts';
 import { createGameWords } from './services/createGameWords';
 
-export default function App() {
-  const [haveGrid, setHaveGrid] = React.useState(false); 
-  const [contacts, setContacts] = React.useState([]);
-  const [grid, setGrid] = React.useState([]);
-  const [gridWords, setGridWords] = React.useState([]);
-  const [scores, setScores] = React.useState([]);
+class App extends Component {
+  state = { 
+    haveGrid: false,
+    contacts: [],
+    grid: [],
+    gridWords: [],
+    scores: [],
+    contactsPermissionsDenied: false
+  }
 
-  const generateCrosswordPressed = () => {
+  componentDidMount() {
+    if(!this.state.contacts[0] && !this.state.haveGrid) {
+      (async () => {
+        const { status } = await Contacts.requestPermissionsAsync();
+        if (status === 'granted') {
+          const { data } = await Contacts.getContactsAsync({
+            fields: [Contacts.Fields.Emails],
+          });
+  
+          if (data.length > 0) {
+            let formattedContacts = trimContactsData(data);
+            this.setState({contacts: formattedContacts});
+            
+            this.generateGame(formattedContacts, 10, 10);
+          }
+        }
+        if (status === 'denied') {
+          this.setState({contactsPermissionsDenied: true})
+        }
+      })();
+    }
+  }
+
+  getContacts = () => {
     (async () => {
       const { status } = await Contacts.requestPermissionsAsync();
       if (status === 'granted') {
@@ -23,50 +49,70 @@ export default function App() {
 
         if (data.length > 0) {
           let formattedContacts = trimContactsData(data);
-          setContacts(formattedContacts);
+          this.setState({contacts: formattedContacts});
           
-          generateGame(formattedContacts, 10, 10);
+          this.generateGame(formattedContacts, 10, 10);
         }
+        this.setState({contactsPermissionsDenied: false})
+      }
+      if (status === 'denied') {
+        this.setState({contactsPermissionsDenied: true})
       }
     })();
   }
 
-  const generateGame = ( formattedContacts, gridRows, gridColumns) => {
+  generateGame = ( formattedContacts, gridRows, gridColumns) => {
     let words = createGameWords(formattedContacts, gridRows, gridColumns);
 
     generatedCrossword = generateCrossword(words, gridRows, gridColumns);
-    setGrid(generatedCrossword[0]);
-    setGridWords(generatedCrossword[1]);
-    setHaveGrid(true);
-
-  }
-
-  const addScore = (score) => {
-    let scoresList = scores;
-    scoresList.push(score);
-    scoresList.sort((a, b) => b - a); // sort scores descending
-    setScores(scoresList);
+    this.setState({grid: generatedCrossword[0]});
+    this.setState({gridWords: generatedCrossword[1]});
+    this.setState({haveGrid: true});
   }
   
-  return (
-    <View style={styles.container}>
-      {haveGrid ? (
-        <CrosswordView 
-          grid={grid}
-          words={gridWords}
-          newGamePressed={() => generateGame(contacts, 10,10)}
-          scores={scores}
-          addScore={addScore}
-        />
-      ) : (
-        <Button 
-          title="Generate Crossword" 
-          onPress={generateCrosswordPressed}
-        />
-      )}
-    </View>
-  );
+  addScore = (score) => {
+    let scoresList = this.state.scores;
+    scoresList.push(score);
+    scoresList.sort((a, b) => b - a); // sort scores descending
+    this.setState({scores: scoresList});
+  }
+
+  render() { 
+    return ( 
+      <View style={styles.container}>
+        {this.state.haveGrid ? (
+          <CrosswordView 
+            grid={this.state.grid}
+            words={this.state.gridWords}
+            newGamePressed={() => this.generateGame(this.state.contacts, 10,10)}
+            scores={this.state.scores}
+            addScore={this.addScore}
+          />
+        ) : (
+          <View>
+            {this.state.contactsPermissionsDenied && (
+              <View style={styles.container}>
+                <Text style={styles.askText}>
+                  Contacts Crossword Requires Access to your contacts in order to create your crosswords. Please go to your settings app and allow contacts access.
+                </Text>
+                <Image style={styles.settingsImage} source={require('./assets/settingsAppIcon.png')} />
+                <Text style={styles.directionsText}>
+                  Settings > Contacts Crossword > Allow Contacts
+                </Text>
+                <Button 
+                  title="OK" 
+                  onPress={this.getContacts}
+                />
+              </View>
+            )}
+          </View>
+        )}
+      </View>
+     );
+  }
 }
+ 
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -75,4 +121,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  settingsImage: {
+    height: 200,
+    width: 200
+  },
+  askText: {
+    fontSize: 22,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15,
+    textAlign: 'center'
+  },
+  directionsText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15
+  }
 });
